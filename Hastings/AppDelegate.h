c8f1edb4c5a89de19fb100924519c933c9385b43
Casey Egan <casey.egan@fcsamerica.com>
2014-07-02 13:34:08 -0500

    Initial Commit

---
+++ Hastings/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hastings
+//
+//  Created by Casey Egan  on 7/2/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
